46005d2eca37ec6a16ac84e7cdc845e405f64b78
Echane Mohamed <110499541+meddch@users.noreply.github.com>
2023-05-01 16:08:26 +0100

    Time: 158 ms (27.10%) | Memory: 14.6 MB (21.79%) - LeetSync

---
+++ contains-duplicate/contains-duplicate.c
@@ -0,0 +1,12 @@
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+bool containsDuplicate(int* nums, int numsSize){
+    qsort(nums, numsSize, sizeof(int), cmpfunc);
+    for(int i=0; i<numsSize-1; i++)
+    {
+        if(nums[i] == nums[i+1]) return true;
+    }
+    return false;
+}
