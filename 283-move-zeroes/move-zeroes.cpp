967c58b8a8a95c2b9146e3878c9949baa1c972f4
Echane Mohamed <110499541+meddch@users.noreply.github.com>
2023-09-15 17:50:52 +0100

    Time: 19 ms (56.00%) | Memory: 19.5 MB (9.68%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums)
+    {
+        int nonZero=0;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[nonZero]);
+                nonZero++;
+            }
+    }
+};
