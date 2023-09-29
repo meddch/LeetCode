5f40c10919d8e4d0a6ef0070f9c80e763adf449c
Echane Mohamed <110499541+meddch@users.noreply.github.com>
2023-09-29 18:51:42 +0100

    Time: 5 ms (33.73%) | Memory: 10.6 MB (8.97%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int arraySign(vector<int>& nums) {
-        int cnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){return 0;break;}
-            if(nums[i]<0) cnt++;
-        }
-        if(cnt%2) return -1;
-        return 1;
+    int arraySign(vector<int>& nums)
+    {
+        if (find(nums.begin(),nums.end(),0) != nums.end())
+            return 0;
+        short res = 1;
+        for(int i = 0; i < nums.size() ;i++)
+            res *=(nums[i] > 0 ? 1 : -1);
+        return res;
     }
 };
