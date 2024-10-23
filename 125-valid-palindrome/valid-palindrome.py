bf0b3d3f08d9520f3b8e4820998abfbc3829b74b
Echane Mohamed <110499541+meddch@users.noreply.github.com>
2024-10-23 16:09:06 +0100

    Time: 739 ms (5.01%) | Memory: 12.9 MB (62.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,8 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        for char in string.punctuation:
-            s = s.replace(char,'')
-        s = s.replace(" ", "")
+        for char in s:
+            if not char.isalnum():
+                s = s.replace(char,'')
         s = s.lower()
         return  s == s[::-1]
